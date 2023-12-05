83d1fd9b78089f721554653d8cff74c6b9e6bcb6
BranBayou <berhan.baye@gmail.com>
2023-12-05 13:54:09 +0300

    Update reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::ReservationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @reservations = @user.reservations.all
-    render json: reservations
+    @reservations = current_user.reservations.all
+    render json: @reservations, status: :ok
   end
 
   def destroy
-    @reservation = @user.reservations.find(params[:id])
+    @reservation = current_user.reservations.find(params[:id])
     if @reservation.destroy
       redirect_to reservations_path, notice: 'Reservation was successfully deleted.'
     else
@@ -15,8 +15,8 @@ class Api::V1::ReservationsController < ApplicationController
     end
   end
 
-  def new
-    @reserve = Reserve.new
+  def create
+    current_user
   end
 
 end
