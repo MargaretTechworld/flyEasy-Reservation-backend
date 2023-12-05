b9e283df3601fdae3e2d7002aa36b69982ca7e98
BranBayou <berhan.baye@gmail.com>
2023-12-05 12:39:49 +0300

    Add destroy method for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,4 +5,14 @@ class ReservationsController < ApplicationController
     @reservations = @user.reservations.all
   end
 
+  def destroy
+    @reservation = @user.reservations.find(params[:id])
+    if @reservation.destroy
+      redirect_to reservations_path, notice: 'Reservation was successfully deleted.'
+    else
+      flash.now[:error] = 'Error: Reservation could not be deleted'
+    end
+  end
+
+
 end
