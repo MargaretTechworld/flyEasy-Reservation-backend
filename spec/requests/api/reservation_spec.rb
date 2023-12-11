1134fd8a52d200e9ea65487f305adef68c63c0c4
zbahati <bahatizirimwabagabo@gmail.com>
2023-12-12 00:37:03 +0200

    check if returns eror when the user is not authenticated

--- spec/requests/api/reservation_spec.rb
+++ spec/requests/api/reservation_spec.rb
@@ -22,6 +22,43 @@ RSpec.describe Api::V1::ReservationsController, type: :controller do
       end
     end
   end
+  describe 'POST #create' do
+    context 'when user is authenticated' do
+      it 'creates a new reservation' do
+        admin_user = User.create(email: 'admin@example.com', password: 'password', name: 'Admin User', admin: true)
+        sign_in admin_user
 
- 
+        meal = Meal.create(name: 'Meal 1', description: 'Delicious meal', price: 10.99, user: admin_user, available: true, photo:"image.jpg")
+
+        reservation_params = {
+          reserve_time: '12:00',
+          quantity: 2,
+          spicy_level: 'medium',
+          reserve_date: '2023-01-01'
+        }
+
+        post :create, params: { meal_id: meal.id, user_id: admin_user.id, reservation: reservation_params }
+        reservation_response = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:ok)
+        expect(reservation_response['id']).not_to be_nil
+      end
+    end
+
+    it 'returns status code 401 if user is not authenticated' do
+      admin_user = User.create(email: 'admin@example.com', password: 'password', name: 'Admin User', admin: true)
+      meal = Meal.create(name: 'Meal 1', description: 'Delicious meal', price: 10.99, user: admin_user, available: true, photo:"image.jpg")
+
+      reservation_params = {
+        reserve_time: '12:00',
+        quantity: 2,
+        spicy_level: 'medium',
+        reserve_date: '2023-01-01'
+      }
+
+      post :create, params: { meal_id: meal.id,  reservation: reservation_params }
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
 end
