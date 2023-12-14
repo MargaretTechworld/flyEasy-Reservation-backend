ad385ba1417f862afb61706b66de666df6d1a889
MargaretTechworld <margarettechworld@gmail.com>
2023-12-14 22:56:07 +0000

    deployment

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.enabled = false
+
 module FlyEasyReservationBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -28,6 +26,10 @@ module FlyEasyReservationBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Disable assets pipeline
+    config.assets.enabled = false
+    
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
