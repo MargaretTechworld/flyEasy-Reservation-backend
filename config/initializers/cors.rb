fe585ae6d9e75eb7a86eab3bbaf309399c34d84c
MargaretTechworld <margarettechworld@gmail.com>
2024-04-23 16:36:01 +0000

    Update EndPoint Linnk

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,13 +5,19 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "*"
+
+#     resource "*",
+#       headers: :any,
+#       expose: ["Authorization"],
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-
-    resource "*",
-      headers: :any,
-      expose: ["Authorization"],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
+    origins 'https://meal-connect-backend.onrender.com/' 
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+  end
